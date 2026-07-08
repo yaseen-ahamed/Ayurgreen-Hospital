72a6e112a4c8469d201800e844b9b9809a09fff6
Yaseen <Yasin@MacBook-Air.local>
2026-07-09 01:19:01 +0530

    fix: change absolute positions in LiquidButton to inset-0 and add weather widget sizing overrides to fix layout

--- components/ui/liquid-glass-button.tsx
+++ components/ui/liquid-glass-button.tsx
@@ -112,12 +112,12 @@ function LiquidButton({
         )}
         {...props}
       >
-        <div className="absolute top-0 left-0 z-0 h-full w-full rounded-full 
+        <div className="absolute inset-0 z-0 rounded-[inherit] 
             shadow-[0_0_6px_rgba(0,0,0,0.03),0_2px_6px_rgba(0,0,0,0.08),inset_3px_3px_0.5px_-3px_rgba(0,0,0,0.9),inset_-3px_-3px_0.5px_-3px_rgba(0,0,0,0.85),inset_1px_1px_1px_-0.5px_rgba(0,0,0,0.6),inset_-1px_-1px_1px_-0.5px_rgba(0,0,0,0.6),inset_0_0_6px_6px_rgba(0,0,0,0.12),inset_0_0_2px_2px_rgba(0,0,0,0.06),0_0_12px_rgba(255,255,255,0.15)] 
         transition-all 
         dark:shadow-[0_0_8px_rgba(0,0,0,0.03),0_2px_6px_rgba(0,0,0,0.08),inset_3px_3px_0.5px_-3.5px_rgba(255,255,255,0.09),inset_-3px_-3px_0.5px_-3.5px_rgba(255,255,255,0.85),inset_1px_1px_1px_-0.5px_rgba(255,255,255,0.6),inset_-1px_-1px_1px_-0.5px_rgba(255,255,255,0.6),inset_0_0_6px_6px_rgba(255,255,255,0.12),inset_0_0_2px_2px_rgba(255,255,255,0.06),0_0_12px_rgba(0,0,0,0.15)]" />
         <div
-          className="absolute top-0 left-0 isolate -z-10 h-full w-full overflow-hidden rounded-md"
+          className="absolute inset-0 isolate -z-10 overflow-hidden rounded-[inherit]"
           style={{ backdropFilter: 'url("#container-glass")' }}
         />
 
